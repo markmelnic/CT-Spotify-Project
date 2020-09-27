46094fce53675b365e637a411e4260944ef1168f
Mark Melnic <markhappy200@gmail.com>
2020-09-27 12:59:42 +0200

    Added week3

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ if __name__ == "__main__":
     playlists, users, user_number = simulation()
     week1(playlists, users, user_number)
     week2(playlists, users, user_number)
+    week3(playlists, users, user_number)
